8275fa4c90ecf1a6d8abaaa952009587f8394fe9
vaesniels <wtdniels@hotmail.com>
2017-11-10 20:37:22 +0100

    Changes to notifications to work on android 7 instead of android 8

--- Universo/app/src/main/java/com/example/maurits/universo/notifications/AlarmNotificationReciever.java
+++ Universo/app/src/main/java/com/example/maurits/universo/notifications/AlarmNotificationReciever.java
@@ -1,15 +1,19 @@
 package com.example.maurits.universo.notifications;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 
 import com.example.maurits.universo.notifications.NotificationHelper;
 
 import java.util.Random;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Created by niels on 24-Oct-17.
@@ -22,7 +26,9 @@ public class AlarmNotificationReciever extends BroadcastReceiver {
 
         NotificationHelper helper;
         helper = new NotificationHelper(context);
-        Notification.Builder builder = helper.getNotification("Universo","Discover the universe with Universo");
-        helper.getManager().notify(new Random().nextInt(),builder.build());
+        NotificationCompat.Builder builder = helper.getNotification("Universo","Discover the universe with Universo");
+        NotificationManager mNotifyMgr =
+                (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(1,builder.build());
     }
 }
--- Universo/app/src/main/java/com/example/maurits/universo/notifications/NotificationHelper.java
+++ Universo/app/src/main/java/com/example/maurits/universo/notifications/NotificationHelper.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 
 import com.example.maurits.universo.R;
 import com.example.maurits.universo.activity.MainActivity;
@@ -20,15 +21,15 @@ import com.example.maurits.universo.activity.StartUpActivity;
  */
 
 public class NotificationHelper extends ContextWrapper {
-    private static final String UNIVERSO_CHANNEL_ID = "com.example.universo";
-    private static final String UNIVERSO_CHANNEL_NAME = "universo Channel";
-    private NotificationManager manager;
+   // private static final String UNIVERSO_CHANNEL_ID = "com.example.universo";
+    //private static final String UNIVERSO_CHANNEL_NAME = "universo Channel";
+    //private NotificationManager manager;
     @RequiresApi(api = Build.VERSION_CODES.O)
     public NotificationHelper(Context base) {
         super(base);
-        createChannels();
+       // createChannels();
     }
-
+/**
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void createChannels() {
         NotificationChannel nielschannel = new NotificationChannel(UNIVERSO_CHANNEL_ID, UNIVERSO_CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT);
@@ -45,19 +46,26 @@ public class NotificationHelper extends ContextWrapper {
             manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         return manager;
     }
+**/
+    //@RequiresApi(api = Build.VERSION_CODES.O)
+    public NotificationCompat.Builder getNotification(String title, String text) {
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public Notification.Builder getNotification(String title, String text) {
-        Notification.Builder builder = new Notification.Builder(getApplicationContext(), UNIVERSO_CHANNEL_ID);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.universeicon)
+                        .setContentTitle(title)
+                        .setContentText(text);
+        mBuilder.setAutoCancel(true);
+        /** Notification.Builder builder = new Notification.Builder(getApplicationContext(), UNIVERSO_CHANNEL_ID);
         builder.setContentTitle(title);
         builder.setContentText(text);
         builder.setSmallIcon(R.drawable.universeicon);
-        builder.setAutoCancel(true);
+        builder.setAutoCancel(true);**/
         //builder.getNotification().flags |= Notification.FLAG_AUTO_CANCEL;
 
         Intent resultIntent = new Intent(this, StartUpActivity.class);
         PendingIntent resultPendingIntent = PendingIntent.getActivity(this,0,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(resultPendingIntent);
-        return builder;
+        mBuilder.setContentIntent(resultPendingIntent);
+        return mBuilder;
     }
 }
